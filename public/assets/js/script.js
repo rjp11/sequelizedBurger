754a998893ce460055c71d753f76bfc0adb60a3b
Ryan Parks <ryan.j.parks12@gmail.com>
2018-03-18 12:54:20 -0500

    updated devoured button js

---
+++ public/assets/js/script.js
@@ -0,0 +1,46 @@
+$(function() {
+    $(".devourIt").on("click", function(event) {
+      var id = $(this).attr("id");
+      
+      var devouredStatus = {
+          devoured: true,
+      } 
+
+      console.log(devouredStatus);
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: devouredStatus
+      }).then(
+        function() {
+          console.log("changed devoured to true");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $("#newBurgerForm").val().trim(),
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+  });
+  
