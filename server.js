a065534868d15431f8eea32d62d57f3205a25c81
Ryan Parks <ryan.j.parks12@gmail.com>
2018-03-18 11:25:27 -0500

    server.js updated based on sequelized instructions step #5. App runs locally.

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path")
+var path = require("path");
+var db = require("./models");
 
 var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(path.join(__dirname, "public")));
+
+
+// listen on port 3000
+// INSERT DIFFERENT PORT LANGUAGE HERE!!!!!
+// AND DROP YOUR OLD BURGER DATABASE TO MAKE LOCAL MACHINE WORK
+var PORT = process.env.PORT || 3000;
+
+db.sequelize.sync().then(function(){
+  app.listen(PORT, function(){
+    console.log("Listening on port %s", PORT);
+  });
+});
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
@@ -23,9 +37,4 @@ app.use("/", routes);
 app.use("/update", routes);
 app.use("/create", routes);
 
-// listen on port 3000
 
-// INSERT DIFFERENT PORT LANGUAGE HERE!!!!!
-// AND DROP YOUR OLD BURGER DATABASE TO MAKE LOCAL MACHINE WORK
-var port = process.env.PORT || 3000;
-app.listen(port);
